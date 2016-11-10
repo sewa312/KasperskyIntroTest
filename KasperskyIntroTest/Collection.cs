7dfdcdedc89f491dcb74da72ac69e834ea69fcec
sewa312 <sewa080@mail.ru>
2016-11-10 23:08:21 +0300

    "Finf all pairs" added

---
+++ KasperskyIntroTest/Collection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasperskyIntroTest
+{
+    class Collection
+    { 
+        private SortedSet<float> set;
+
+        public Collection()
+        {
+            set = new SortedSet<float>();
+        }
+
+        public void Add(float item)
+        {
+            set.Add(item);
+        }
+
+        public void FindAllPairs(float x)
+        {
+            float[] collect = new float[set.Count];
+            set.CopyTo(collect);
+            for (int i = 0; i < collect.Length; i++)
+            {
+                if (set.Contains(x - collect[i]) && collect[i] >= (x - collect[i]))
+                    Console.WriteLine("{0} = {1} + {2}", x, x - collect[i], collect[i]);
+            }
+        } 
+    }
+}
--- KasperskyIntroTest/Program.cs
+++ KasperskyIntroTest/Program.cs
@@ -10,6 +10,19 @@ namespace KasperskyIntroTest
     {
         static void Main(string[] args)
         {
+            Collection set = new Collection();
+            set.Add(2);
+            set.Add(1);
+            set.Add(3);
+            set.Add(4);
+            set.Add(5);
+            set.Add(6);
+            set.Add(7);
+            set.Add(9);
+            set.Add(10);
+            set.Add(11);
+            set.FindAllPairs(20);
+            Console.ReadLine();
         }
     }
 }
